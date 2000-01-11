3974eda170ced83c7e8c05d867d41a2e1d6a7b9e
nop <nop>
2000-01-11 02:15:09 +0000

    Release.

--- version.c
+++ version.c
@@ -40,7 +40,7 @@
 #include "config.h"
 #include "version.h"
 
-const char *server_version = "1.8.0r8";
+const char *server_version = "1.8.1";
 
 int
 check_version(DB_Version version)
@@ -48,4 +48,4 @@ check_version(DB_Version version)
     return version < Num_DB_Versions;
 }
 
-char rcsid_version[] = "$Id: version.c,v 1.9 1999/08/18 01:09:59 nop Exp $";
+char rcsid_version[] = "$Id: version.c,v 1.10 2000/01/11 02:15:09 nop Exp $";
