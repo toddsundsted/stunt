52a5491a7a97775c693c91e0c5fc710c6a655dcb
Todd Sundsted <todd@sundsted.com>
2010-07-10 17:33:47 -0400

    Added request/response functions to access the HTTP connection.

--- httpd.c
+++ httpd.c
@@ -20,12 +20,44 @@
 
 struct MHD_Daemon *mhd_daemon;
 
+typedef unsigned32 Connid;
+
 struct connection_info_struct
 {
-  int connectiontype;
-  Objid listener;
-  char *echostring;
+  Connid id;
+  Objid player;
+  char *request_type;
+  char *request_body;
+  char *response_type;
+  char *response_body;
 };
+
+static Connid max_id = 0;
+
+static struct connection_info_struct *all_con_info = NULL;
+
+struct connection_info_struct * find_connection_info_struct(Connid id)
+{
+  return all_con_info->id == id ? all_con_info : NULL;
+}
+
+void remove_connection_info_struct(struct connection_info_struct *con_info)
+{
+  if (con_info != NULL) {
+    if (con_info->request_type) free(con_info->request_type);
+    if (con_info->request_body) free(con_info->request_body);
+    if (con_info->response_type) free(con_info->response_type);
+    if (con_info->response_body) free(con_info->response_body);
+    free(con_info);
+  }
+  all_con_info = NULL;
+}
+
+void add_connection_info_struct(struct connection_info_struct *con_info)
+{
+  remove_connection_info_struct(all_con_info);
+  all_con_info = con_info;
+}
 
 int ask_for_authentication(struct MHD_Connection *connection, const char *realm)
 {
@@ -105,7 +137,7 @@ int is_authenticated(struct MHD_Connection *connection, struct connection_info_s
   outcome = run_server_task(-1, SYSTEM_OBJECT, "authenticate", args, "", &result);
 
   if (outcome == OUTCOME_DONE && result.type == TYPE_OBJ) {
-    con_info->listener = result.v.obj;
+    con_info->player = result.v.obj;
     authenticated = 1;
   }
 
@@ -117,17 +149,40 @@ int is_authenticated(struct MHD_Connection *connection, struct connection_info_s
 }
 
 static void
+dir_free_callback (void *cls)
+{
+  oklog("DIR FREE CALLBACK!\n");
+}
+
+static int
+dir_reader (void *cls, uint64_t pos, char *buf, int max)
+{
+  oklog("DIR READER!\n");
+
+  struct connection_info_struct *con_info = (struct connection_info_struct *)cls;
+
+  if (con_info->response_type && con_info->response_body) {
+    printf("pos = %d\n", pos);
+    printf("max = %d\n", max);
+    char * foo = strncpy(buf, con_info->response_body + pos, max);
+    int len = strlen(foo);
+    printf("len = %d\n", len);
+    return len ? len : -1;
+  }
+  else {
+    return 0;
+  }
+}
+
+static void
 request_completed (void *cls,
 		   struct MHD_Connection *connection,
 		   void **ptr,
 		   enum MHD_RequestTerminationCode toe)
 {
-  struct connection_info_struct *con_info = (struct connection_info_struct *) *ptr;
+  oklog("REQUEST COMPLETED!\n");
 
-  if (NULL == con_info) return;
-  if (con_info->echostring) free (con_info->echostring);
-  free (con_info);
-  *ptr = NULL;   
+  remove_connection_info_struct(*ptr);
 }
 
 static int
@@ -138,86 +193,81 @@ ahc_echo (void *cls,
           const char *version,
           const char *upload_data, size_t *upload_data_size, void **ptr)
 {
-  const char *page = "";
-  struct MHD_Response *response;
-  int ret;
-  Var args;
-
   struct connection_info_struct *con_info;
 
-  if(NULL == *ptr) 
-    {
-      con_info = malloc (sizeof (struct connection_info_struct));
+  if (NULL == *ptr) {
+      con_info = malloc(sizeof(struct connection_info_struct));
       if (NULL == con_info) return MHD_NO;
-      con_info->listener = -1;
-      con_info->echostring = NULL;
-      *ptr = (void *) con_info; 
+      con_info->id = ++max_id;
+      con_info->player = NOTHING;
+      con_info->request_type = NULL;
+      con_info->request_body = NULL;
+      con_info->response_type = NULL;
+      con_info->response_body = NULL;
+      *ptr = (void *)con_info;
+      add_connection_info_struct(con_info);
       return MHD_YES;
     }
 
   con_info = *ptr;
       
   if (!is_authenticated (connection, con_info))
-    return ask_for_authentication (connection, "lambdaMoo");
+    return ask_for_authentication (connection, "Moo");
 
   if (*upload_data_size != 0)
     {
-      if (con_info->echostring)
+      if (con_info->request_body)
 	{
-	  char *oldstring = con_info->echostring;
+	  char *oldstring = con_info->request_body;
 	  size_t len = strlen(oldstring) + *upload_data_size + 1;
-	  con_info->echostring = malloc(len);
-	  con_info->echostring[0] = 0;
-	  strcat(con_info->echostring, oldstring);
-	  strncat(con_info->echostring, upload_data, *upload_data_size);
-	  con_info->echostring[len - 1] = 0;
+	  con_info->request_body = malloc(len);
+	  con_info->request_body[0] = 0;
+	  strcat(con_info->request_body, oldstring);
+	  strncat(con_info->request_body, upload_data, *upload_data_size);
+	  con_info->request_body[len - 1] = 0;
 	  free (oldstring);
 	}
       else
 	{
 	  size_t len = *upload_data_size + 1;
-	  con_info->echostring = malloc(len);
-	  con_info->echostring[0] = 0;
-	  strncat(con_info->echostring, upload_data, *upload_data_size);
-	  con_info->echostring[len - 1] = 0;
+	  con_info->request_body = malloc(len);
+	  con_info->request_body[0] = 0;
+	  strncat(con_info->request_body, upload_data, *upload_data_size);
+	  con_info->request_body[len - 1] = 0;
 	}
 
       *upload_data_size = 0;
       return MHD_YES;
     }
-  else
-    {
-    }
 
+  const char *content_type;
+
+  content_type = MHD_lookup_connection_value (connection, MHD_HEADER_KIND, "Content-Type");
+
+  con_info->request_type = content_type ? strdup(content_type) : strdup("");
+
+  Var args;
   Var result;
   enum outcome outcome;
+  int ret;
 
-  if (con_info->echostring)
-    {
-      int len = strlen(con_info->echostring);
-      args = new_list(1);
-      args.v.list[1].type = TYPE_STR;
-      args.v.list[1].v.str = str_dup(raw_bytes_to_binary(con_info->echostring, len));
-      outcome = run_server_task(con_info->listener, SYSTEM_OBJECT, "do_httpd", args, "", &result);
-    }
-  else
-    {
-      outcome = run_server_task(con_info->listener, SYSTEM_OBJECT, "do_httpd", new_list(0), "", &result);
-    }
+  args = new_list(1);
+  args.v.list[1].type = TYPE_INT;
+  args.v.list[1].v.num = con_info->id;
+  outcome = run_server_task(con_info->player, SYSTEM_OBJECT, "do_httpd", args, "", &result);
 
-  if (outcome == OUTCOME_DONE)
-    {
-      char * foo = value_to_literal(result);
-      response = MHD_create_response_from_data (strlen (foo), (void *) foo, MHD_NO, MHD_NO);
-      ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
-      MHD_destroy_response (response);
-    }
-  else
-    {
-      response = MHD_create_response_from_data (strlen (page), (void *) page, MHD_NO, MHD_NO);
-      ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
-      MHD_destroy_response (response);
-    }
+  //  if (outcome == OUTCOME_DONE || outcome == OUTCOME_ABORTED)
+  //    {
+  //    }
+  //  else
+  //    {
+  //    }
+
+  struct MHD_Response *response;
+
+  response = MHD_create_response_from_callback (MHD_SIZE_UNKNOWN, 32 * 1024, &dir_reader, con_info, &dir_free_callback); 
+  ret = MHD_queue_response (connection, MHD_HTTP_OK, response);
+  MHD_destroy_response (response);
 
   free_var(result);
 
@@ -266,9 +316,98 @@ bf_stop_httpd_server(Var arglist, Byte next, void *vdata, Objid progr)
     return no_var_pack();
 }
 
+static package
+bf_request(Var arglist, Byte next, void *vdata, Objid progr)
+{
+  Connid id = arglist.v.list[1].v.num;
+  char *opt = arglist.v.list[2].v.str;
+
+  if (0 != strcmp(opt, "type") && 0 != strcmp(opt, "body")) {
+    free_var(arglist);
+    return make_error_pack(E_INVARG);
+  }
+
+  if (!is_wizard(progr)) {
+    free_var(arglist);
+    return make_error_pack(E_PERM);
+  }
+
+  struct connection_info_struct *con_info = find_connection_info_struct(id);
+
+  if (con_info && 0 == strcmp(opt, "body") && con_info->request_body) {
+    Var r;
+    int len = strlen(con_info->request_body);
+    r.type = TYPE_STR;
+    r.v.str = str_dup(raw_bytes_to_binary(con_info->request_body, len));
+    free_var(arglist);
+    return make_var_pack(r);
+  }
+  else if (con_info && 0 == strcmp(opt, "body")) {
+    Var r;
+    r.type = TYPE_STR;
+    r.v.str = str_dup("");
+    free_var(arglist);
+    return make_var_pack(r);
+  }
+  else if (con_info && 0 == strcmp(opt, "type") && con_info->request_type) {
+    Var r;
+    r.type = TYPE_STR;
+    r.v.str = str_dup(con_info->request_type);
+    free_var(arglist);
+    return make_var_pack(r);
+  }
+  else if (con_info && 0 == strcmp(opt, "type")) {
+    Var r;
+    r.type = TYPE_STR;
+    r.v.str = str_dup("");
+    free_var(arglist);
+    return make_var_pack(r);
+  }
+  else {
+    free_var(arglist);
+    return make_error_pack(E_INVARG);
+  }
+}
+
+static package
+bf_response(Var arglist, Byte next, void *vdata, Objid progr)
+{
+  Connid id = arglist.v.list[1].v.num;
+  char *opt = arglist.v.list[2].v.str;
+
+  if (0 != strcmp(opt, "type") && 0 != strcmp(opt, "body")) {
+    free_var(arglist);
+    return make_error_pack(E_INVARG);
+  }
+
+  if (!is_wizard(progr)) {
+    free_var(arglist);
+    return make_error_pack(E_PERM);
+  }
+
+  struct connection_info_struct *con_info = find_connection_info_struct(id);
+
+  if (con_info && 0 == strcmp(opt, "body") && con_info->response_body == NULL) {
+    con_info->response_body = strdup(value_to_literal(arglist.v.list[3]));
+    free_var(arglist);
+    return no_var_pack();
+  }
+  else if (con_info && 0 == strcmp(opt, "type") && con_info->response_type == NULL) {
+    con_info->response_type = strdup(value_to_literal(arglist.v.list[3]));
+    free_var(arglist);
+    return no_var_pack();
+  }
+  else {
+    free_var(arglist);
+    return make_error_pack(E_INVARG);
+  }
+}
+
 void
 register_httpd(void)
 {
     register_function("start_httpd_server", 0, 0, bf_start_httpd_server);
     register_function("stop_httpd_server", 0, 0, bf_stop_httpd_server);
+    register_function("request", 2, 2, bf_request, TYPE_INT, TYPE_STR);
+    register_function("response", 3, 3, bf_response, TYPE_INT, TYPE_STR, TYPE_ANY);
 }
