198fadaec563b2ca23bf72b1780b303c9376de7b
Todd Sundsted <todd@sundsted.com>
2010-07-26 08:11:48 -0400

    Initial commit of the FileIO patch.

---
+++ extension-fileio.h
@@ -0,0 +1,15 @@
+/*
+ * extension-fileio.h 
+ *
+ */
+
+#ifndef EXT_FILE_IO_H
+
+#define EXT_FILE_IO_H 1
+
+#define FILE_IO_MAX_FILES 256
+#define FILE_SUBDIR "files/"
+
+#define FILE_IO_BUFFER_LENGTH 4096
+
+#endif
