21ac73c6529f5ae51ae930b685cb2a4067ddf017
Todd Sundsted <todd@sundsted.com>
2010-08-30 13:39:51 -0400

    Major exec features are completed.

--- exec.c
+++ exec.c
@@ -48,11 +48,35 @@ typedef struct tasks_waiting_on_exec {
   int in;
   int out;
   int err;
+  Stream *sout;
+  Stream *serr;
   vm the_vm;
   UT_hash_handle hh;
 } tasks_waiting_on_exec;
 
 static tasks_waiting_on_exec *exec_waiters = NULL;
+
+static tasks_waiting_on_exec *
+malloc_tasks_waiting_on_exec()
+{
+  tasks_waiting_on_exec *tw = mymalloc(sizeof(tasks_waiting_on_exec), M_TASK);
+  tw->sout = new_stream(1000);
+  tw->serr = new_stream(1000);
+  return tw;
+}
+
+static void
+free_tasks_waiting_on_exec(tasks_waiting_on_exec *tw)
+{
+  free_stream(tw->sout);
+  free_stream(tw->serr);
+  /* close(tw->in); already closed */
+  network_unregister_fd(tw->out);
+  network_unregister_fd(tw->err);
+  close(tw->out);
+  close(tw->err);
+  myfree(tw, M_TASK);
+}
 
 static task_enum_action
 exec_waiter_enumerator(task_closure closure, void *data)
@@ -64,10 +88,7 @@ exec_waiter_enumerator(task_closure closure, void *data)
     task_enum_action tea = (*closure)(tw->the_vm, status, data);
     if (TEA_KILL == tea) {
       HASH_DELETE(hh, exec_waiters, tw);
-      close(tw->in);
-      close(tw->out);
-      close(tw->err);
-      myfree(tw, M_TASK);
+      free_tasks_waiting_on_exec(tw);
     }
     if (TEA_CONTINUE != tea)
       return tea;
@@ -75,22 +96,63 @@ exec_waiter_enumerator(task_closure closure, void *data)
   return TEA_CONTINUE;
 }
 
+static void
+write_all(int fd, const char *buffer)
+{
+  size_t length;
+  ssize_t count;
+  length = strlen(buffer);
+  while (length) {
+    count = write(fd, buffer, length);
+    buffer += count;
+    length -= count;
+  }
+}
+
+static void
+stdout_readable(int fd, void *data)
+{
+  tasks_waiting_on_exec *tw = data;
+  char buffer[1000];
+  int n;
+  while ((n = read(fd, buffer, sizeof(buffer))) > 0) {
+    buffer[n] = '\0';
+    stream_add_string(tw->sout, raw_bytes_to_binary(buffer, strlen(buffer)));
+  }
+}
+
+static void
+stderr_readable(int fd, void *data)
+{
+  tasks_waiting_on_exec *tw = data;
+  char buffer[1000];
+  int n;
+  while ((n = read(fd, buffer, sizeof(buffer))) > 0) {
+    buffer[n] = '\0';
+    stream_add_string(tw->serr, raw_bytes_to_binary(buffer, strlen(buffer)));
+  }
+}
+
 static enum error
 exec_waiter_suspender(vm the_vm, void *data)
 {
-    tasks_waiting_on_exec *tw = data;
-    tw->the_vm = the_vm;
-    HASH_ADD(hh, exec_waiters, p, sizeof(pid_t), tw);
-    return E_NONE;
+  tasks_waiting_on_exec *tw = data;
+  network_register_fd(tw->out, stdout_readable, NULL, tw);
+  network_register_fd(tw->err, stderr_readable, NULL, tw);
+  tw->the_vm = the_vm;
+  HASH_ADD(hh, exec_waiters, p, sizeof(pid_t), tw);
+  return E_NONE;
 }
 
 static package
 bf_exec(Var arglist, Byte next, void *vdata, Objid progr)
 {
+  /* The first argument must be a list of strings with at least one
+   * string (the command).
+   */
   int i;
-
-  for (i = 1; i <= arglist.v.list[0].v.num; i++) {
-    if (arglist.v.list[i].type != TYPE_STR) {
+  for (i = 1; i <= arglist.v.list[1].v.list[0].v.num; i++) {
+    if (arglist.v.list[1].v.list[i].type != TYPE_STR) {
       free_var(arglist);
       return make_error_pack(E_INVARG);
     }
@@ -99,46 +161,48 @@ bf_exec(Var arglist, Byte next, void *vdata, Objid progr)
     free_var(arglist);
     return make_error_pack(E_ARGS);
   }
+  const char *args[i];
+  for (i = 1; i <= arglist.v.list[1].v.list[0].v.num; i++) {
+    int len;
+    args[i - 1] = binary_to_raw_bytes(arglist.v.list[1].v.list[i].v.str, &len);
+  }
+  args[i - 1] = NULL;
 
-  const char *cmd = arglist.v.list[1].v.str;
-
-  if (1 < strlen(cmd) && '.' == cmd[0] && '.' == cmd[1]) {
+  /* Check the path. */
+  const char *cmd = args[0];
+  if (0 == strlen(cmd)) {
     free_var(arglist);
     return make_raise_pack(E_INVARG, "Invalid path", zero);
   }
-  if (strstr(cmd, "/.")) {
+  if (('/' == cmd[0]) || (1 < strlen(cmd) && '.' == cmd[0] && '.' == cmd[1])) {
+    free_var(arglist);
+    return make_raise_pack(E_INVARG, "Invalid path", zero);
+  }
+  if (strstr(cmd, "/.") || strstr(cmd, "./")) {
     free_var(arglist);
     return make_raise_pack(E_INVARG, "Invalid path", zero);
   }
 
+  /* Prepend the bin subdirectory path. */
   static Stream *s;
-
-  if(!s)
+  if (!s)
     s = new_stream(0);
   stream_add_string(s, BIN_SUBDIR);
-  if('/' == cmd[0])
-    stream_add_string(s, cmd + 1);
-  else
-    stream_add_string(s, cmd);
-  
+  stream_add_string(s, cmd);
   cmd = reset_stream(s);
 
+  /* Stat the command. */
   struct stat buf;
-
-  if(stat(cmd, &buf) != 0) {
+  if (stat(cmd, &buf) != 0) {
     free_var(arglist);
     return make_raise_pack(E_INVARG, "Does not exist", zero);
   }
-
-  const char *args[i];
-  
-  for (i = 1; i <= arglist.v.list[0].v.num; i++) {
-    args[i - 1] = arglist.v.list[i].v.str;
+  if (!S_ISREG(buf.st_mode)) {
+    free_var(arglist);
+    return make_raise_pack(E_INVARG, "Is not a file", zero);
   }
 
-  args[i - 1] = NULL;
-  args[0] = cmd;
-
+  /* Create pipes, fork, and exec. */
   pid_t p;
   int pipeIn[2];
   int pipeOut[2];
@@ -180,15 +244,15 @@ bf_exec(Var arglist, Byte next, void *vdata, Objid progr)
     /* child */
     int status;
     if ((status = dup2(pipeIn[0], STDIN_FILENO)) < 0) {
-      log_perror("EXEC: ouldn't dup2");
+      perror("dup2");
       exit(status);
     }
     if ((status = dup2(pipeOut[1], STDOUT_FILENO)) < 0) {
-      log_perror("EXEC: Couldn't dup2");
+      perror("dup2");
       exit(status);
     }
     if ((status = dup2(pipeErr[1], STDERR_FILENO)) < 0) {
-      log_perror("EXEC: Couldn't dup2");
+      perror("dup2");
       exit(status);
     }
     close(pipeIn[1]);
@@ -196,21 +260,26 @@ bf_exec(Var arglist, Byte next, void *vdata, Objid progr)
     close(pipeErr[0]);
     static char *env[] = { "PATH=/bin:/usr/bin", NULL };
     int res = execve(cmd, (char *const *)args, (char *const *)env);
-    log_perror("EXEC: Executing %s failed with error code %d...\n", cmd, res);
+    perror("execve");
     exit(res);
   }
   else {
     /* parent */
-    close(pipeIn[0]);
-    close(pipeOut[1]);
-    close(pipeErr[1]);
-    tasks_waiting_on_exec *tw = mymalloc(sizeof(tasks_waiting_on_exec), M_TASK);
+    oklog("EXEC: Executing %s...\n", cmd);
+    tasks_waiting_on_exec *tw = malloc_tasks_waiting_on_exec();
     tw->p = p;
     tw->in = pipeIn[1];
     tw->out = pipeOut[0];
     tw->err = pipeErr[0];
+    close(pipeIn[0]);
+    close(pipeOut[1]);
+    close(pipeErr[1]);
+    if (arglist.v.list[0].v.num > 1) {
+      int len;
+      write_all(tw->in, binary_to_raw_bytes(arglist.v.list[2].v.str, &len));
+    }
+    close(tw->in);
     free_var(arglist);
-    oklog("EXEC: Executing %s...\n", cmd);
     return make_suspend_pack(exec_waiter_suspender, tw);
   }
 
@@ -225,33 +294,21 @@ exec_completed(pid_t p, int code)
 
   HASH_FIND(hh, exec_waiters, &p, sizeof(pid_t), tw);
   if (tw) {
-    char buffer[1000];
-    int n;
-
     Var v;
     v = new_list(3);
     v.v.list[1].type = TYPE_INT;
     v.v.list[1].v.num = code;
-
-    n = read(tw->out, buffer, sizeof(buffer));
-    buffer[n] = '\0';
-
+    stdout_readable(tw->out, tw);
     v.v.list[2].type = TYPE_STR;
-    v.v.list[2].v.str = str_dup(buffer);
-
-    n = read(tw->err, buffer, sizeof(buffer));
-    buffer[n] = '\0';
-
+    v.v.list[2].v.str = str_dup(reset_stream(tw->sout));
+    stderr_readable(tw->err, tw);
     v.v.list[3].type = TYPE_STR;
-    v.v.list[3].v.str = str_dup(buffer);
+    v.v.list[3].v.str = str_dup(reset_stream(tw->serr));
 
     resume_task(tw->the_vm, v);
 
     HASH_DELETE(hh, exec_waiters, tw);
-    close(tw->in);
-    close(tw->out);
-    close(tw->err);
-    myfree(tw, M_TASK);
+    free_tasks_waiting_on_exec(tw);
 
     return p;
   }
@@ -263,5 +320,5 @@ void
 register_exec(void)
 {
   register_task_queue(exec_waiter_enumerator);
-  register_function("exec", 0, -1, bf_exec);
+  register_function("exec", 1, 2, bf_exec, TYPE_LIST, TYPE_STR);
 }
