1450915941efc3a51e262c1b42b8e92c8bb022bc
Todd Sundsted <todd@sundsted.com>
2011-07-24 10:06:37 -0400

    Whitespace changes.

--- base64.c
+++ base64.c
@@ -52,7 +52,7 @@
 #include "server.h"
 
 static const unsigned char base64_table[64] =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 /**
  * base64_encode - Base64 encode
@@ -69,43 +69,43 @@ static const unsigned char base64_table[64] =
 unsigned char *
 base64_encode(const unsigned char *src, size_t len, size_t *out_len)
 {
-	unsigned char *out, *pos;
-	const unsigned char *end, *in;
-	size_t olen;
+    unsigned char *out, *pos;
+    const unsigned char *end, *in;
+    size_t olen;
 
-	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
-	olen++; /* nul termination */
-	out = mymalloc(olen, M_STRING);
-	if (out == NULL)
-		return NULL;
+    olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+    olen++; /* nul termination */
+    out = mymalloc(olen, M_STRING);
+    if (out == NULL)
+	return NULL;
 
-	end = src + len;
-	in = src;
-	pos = out;
-	while (end - in >= 3) {
-		*pos++ = base64_table[in[0] >> 2];
-		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
-		*pos++ = base64_table[in[2] & 0x3f];
-		in += 3;
+    end = src + len;
+    in = src;
+    pos = out;
+    while (end - in >= 3) {
+	*pos++ = base64_table[in[0] >> 2];
+	*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+	*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+	*pos++ = base64_table[in[2] & 0x3f];
+	in += 3;
+    }
+
+    if (end - in) {
+	*pos++ = base64_table[in[0] >> 2];
+	if (end - in == 1) {
+	    *pos++ = base64_table[(in[0] & 0x03) << 4];
+	    *pos++ = '=';
+	} else {
+	    *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+	    *pos++ = base64_table[(in[1] & 0x0f) << 2];
 	}
+	*pos++ = '=';
+    }
 
-	if (end - in) {
-		*pos++ = base64_table[in[0] >> 2];
-		if (end - in == 1) {
-			*pos++ = base64_table[(in[0] & 0x03) << 4];
-			*pos++ = '=';
-		} else {
-			*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
-			*pos++ = base64_table[(in[1] & 0x0f) << 2];
-		}
-		*pos++ = '=';
-	}
-
-	*pos = '\0';
-	if (out_len)
-		*out_len = pos - out;
-	return out;
+    *pos = '\0';
+    if (out_len)
+	*out_len = pos - out;
+    return out;
 }
 
 /**
@@ -121,54 +121,54 @@ base64_encode(const unsigned char *src, size_t len, size_t *out_len)
 unsigned char *
 base64_decode(const unsigned char *src, size_t len, size_t *out_len)
 {
-	unsigned char dtable[256], *out, *pos, in[4], block[4], tmp;
-	size_t i, count, olen;
+    unsigned char dtable[256], *out, *pos, in[4], block[4], tmp;
+    size_t i, count, olen;
 
-	memset(dtable, 0x80, 256);
-	for (i = 0; i < sizeof(base64_table); i++)
-		dtable[base64_table[i]] = i;
-	dtable['='] = 0;
+    memset(dtable, 0x80, 256);
+    for (i = 0; i < sizeof(base64_table); i++)
+	dtable[base64_table[i]] = i;
+    dtable['='] = 0;
 
-	count = 0;
-	for (i = 0; i < len; i++) {
-		if (dtable[src[i]] != 0x80)
-			count++;
+    count = 0;
+    for (i = 0; i < len; i++) {
+	if (dtable[src[i]] != 0x80)
+	    count++;
+    }
+
+    if (count % 4)
+	return NULL;
+
+    olen = count / 4 * 3;
+    pos = out = mymalloc(olen + 1, M_STRING);
+    if (out == NULL)
+	return NULL;
+
+    count = 0;
+    for (i = 0; i < len; i++) {
+	tmp = dtable[src[i]];
+	if (tmp == 0x80)
+	    continue;
+
+	in[count] = src[i];
+	block[count] = tmp;
+	count++;
+	if (count == 4) {
+	    *pos++ = (block[0] << 2) | (block[1] >> 4);
+	    *pos++ = (block[1] << 4) | (block[2] >> 2);
+	    *pos++ = (block[2] << 6) | block[3];
+	    count = 0;
 	}
+    }
 
-	if (count % 4)
-		return NULL;
+    if (pos > out) {
+	if (in[2] == '=')
+	    pos -= 2;
+	else if (in[3] == '=')
+	    pos--;
+    }
 
-	olen = count / 4 * 3;
-	pos = out = mymalloc(olen + 1, M_STRING);
-	if (out == NULL)
-		return NULL;
-
-	count = 0;
-	for (i = 0; i < len; i++) {
-		tmp = dtable[src[i]];
-		if (tmp == 0x80)
-			continue;
-
-		in[count] = src[i];
-		block[count] = tmp;
-		count++;
-		if (count == 4) {
-			*pos++ = (block[0] << 2) | (block[1] >> 4);
-			*pos++ = (block[1] << 4) | (block[2] >> 2);
-			*pos++ = (block[2] << 6) | block[3];
-			count = 0;
-		}
-	}
-
-	if (pos > out) {
-		if (in[2] == '=')
-			pos -= 2;
-		else if (in[3] == '=')
-			pos--;
-	}
-
-	*out_len = pos - out;
-	return out;
+    *out_len = pos - out;
+    return out;
 }
 
 /**** built in functions ****/
