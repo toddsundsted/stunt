16498761601f65085ae0b8727ab0ef646e68671a
Todd Sundsted <todd@sundsted.com>
2013-02-04 22:09:40 -0500

    Added tests for division/modulus.

--- test/test_math.rb
+++ test/test_math.rb
@@ -23,4 +23,50 @@ class TestMath < Test::Unit::TestCase
     end
   end
 
+  def test_that_division_by_zero_fails
+    run_test_as('programmer') do
+      assert_equal E_DIV, simplify(command(%Q|; 1.1 / 0.0; |))
+      assert_equal E_DIV, simplify(command(%Q|; 1 / 0; |))
+      assert_equal E_DIV, simplify(command(%Q|; 1.1 % 0.0; |))
+      assert_equal E_DIV, simplify(command(%Q|; 1 % 0; |))
+    end
+  end
+
+  def test_the_minint_edge_case
+    run_test_as('programmer') do
+      assert_equal -2147483648, simplify(command(%Q|; return -2147483648 / -1; |))
+      assert_equal 0, simplify(command(%Q|; return -2147483648 % -1; |))
+    end
+  end
+
+  def test_division
+    run_test_as('programmer') do
+      assert_equal 5, simplify(command(%Q|; return -15 / -3; |))
+      assert_equal -5, simplify(command(%Q|; return -15 / 3; |))
+      assert_equal -5, simplify(command(%Q|; return 15 / -3; |))
+      assert_equal 5, simplify(command(%Q|; return 15 / 3; |))
+    end
+    run_test_as('programmer') do
+      assert_equal 5.0, simplify(command(%Q|; return -15.0 / -3.0; |))
+      assert_equal -5.0, simplify(command(%Q|; return -15.0 / 3.0; |))
+      assert_equal -5.0, simplify(command(%Q|; return 15.0 / -3.0; |))
+      assert_equal 5.0, simplify(command(%Q|; return 15.0 / 3.0; |))
+    end
+  end
+
+  def test_modulus
+    run_test_as('programmer') do
+      assert_equal -3, simplify(command(%Q|; return -15 % -4; |))
+      assert_equal -3, simplify(command(%Q|; return -15 % 4; |))
+      assert_equal 3, simplify(command(%Q|; return 15 % -4; |))
+      assert_equal 3, simplify(command(%Q|; return 15 % 4; |))
+    end
+    run_test_as('programmer') do
+      assert_equal -3.0, simplify(command(%Q|; return -15.0 % -4.0; |))
+      assert_equal -3.0, simplify(command(%Q|; return -15.0 % 4.0; |))
+      assert_equal 3.0, simplify(command(%Q|; return 15.0 % -4.0; |))
+      assert_equal 3.0, simplify(command(%Q|; return 15.0 % 4.0; |))
+    end
+  end
+
 end
